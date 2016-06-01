5638da28e0d956980bd6871a40fd00e0a5b6c970
Iliyana Sirachka <i.sirachka@gmail.com>
2016-06-02 01:13:41 +0300

    Added some files

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var type = Console.ReadLine().ToLower();
+            var row = int.Parse(Console.ReadLine());
+            var col = int.Parse(Console.ReadLine());
+            var price = -1.0;
+
+            switch (type)
+            {
+                case "premiere":
+                    price = 12;
+                    break;
+                case "normal":
+                    price = 7.5;
+                    break;
+                case "discount":
+                    price = 5;
+                    break;
+            }
+
+            if (price > 0)
+            {
+                var result = row * col * price;
+                Console.WriteLine("{0:f2}", result);
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+        }
+    }
+}
